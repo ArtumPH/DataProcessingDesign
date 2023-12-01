3e8943e2ea6cc83b74fda68d9e866ced514e92bf
ArtumPH <36268014+ArtumPH@users.noreply.github.com>
2023-12-01 17:19:44 +0800

    Delete SelfDefFormatter.py

--- SelfDefFormatter.py
+++
@@ -1,7 +0,0 @@
-
-def y_update_scale_value(temp,position):
-    result=temp/1000
-    return "{}K".format(int(result))
-                        
-def x_update_scale_value(temp,position):
-    return "{}d".format(int(temp))
